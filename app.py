22fd2706ea1f49fcffb1aac2d95cabb248f809be
j13s3 <jalsha.13@gmail.com>
2023-06-19 21:32:39 +1000

    Modified SQL_DB.ipynb to have Primary Key

---
+++ app.py
@@ -0,0 +1,49 @@
+""" Flask application that runs the API and renders the html page(s) """
+import sqlalchemy
+
+from flask import Flask, render_template, jsonify, request
+
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, or_
+from sqlalchemy.ext.automap import automap_base
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+from flask_cors import CORS
+
+# Setup the database
+engine = create_engine("sqlite:///Picnic_Database.db")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+
+Base.prepare(autoload_with=engine)
+
+# session = Session(engine)
+
+print(Base.classes.keys())
+
+picnic_places = Base.classes.Picnic_Data
+
+# Spin up flask app
+app = Flask(__name__)
+
+engine = 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Picnic_Database.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html", title = "Test")
+
+# You need this - this allows you to actually run the app
+if __name__ == "__main__":
+    app.run(debug=True)
