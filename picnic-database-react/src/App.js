8fb1a3f29846497dcb52199639c75faccf8ee07a
owenjohannes <owen_johannes@Owens-MacBook-Pro.local>
2023-06-20 17:11:37 +1000

    V1

---
+++ picnic-database-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DataFetchingComponent from './DataFetchingComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>Your App Title</h1>
+      <DataFetchingComponent />
+    </div>
+  );
+}
+
+export default App;
+
