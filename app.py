42fe485dc7fdeea58c947252514f428487289c54
j13s3 <jalsha.13@gmail.com>
2023-06-20 18:17:31 +1000

    Modifying app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 """ Flask application that runs the API and renders the html page(s) """
+
+import json
 import sqlalchemy
 
 from flask import Flask, render_template, jsonify, request
@@ -25,8 +27,6 @@ Base = automap_base()
 
 Base.prepare(autoload_with=engine)
 
-# session = Session(engine)
-
 print(Base.classes.keys())
 
 picnic_places = Base.classes.Picnic_Spots
@@ -40,7 +40,34 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return "index.html"
+    return render_template("index.html", title = "Group 2; Project 3")
+
+@app.route("/Owens_Stuff")
+def home():
+    return render_template("Owen.html")
+
+@app.route("/Sandeeps_Stuff")
+def home():
+    return render_template("Sandeep.html")
+
+# Flask
+session = Session(engine)
+
+@app.route('/api/json_data', methods= ['GET'])
+def get_data():
+    json_data = session.query(picnic_places).all()
+    data = []
+    for row in json_data:
+        data.append({'id': row.id, 'Amenity': row.Amenity, 'Suburb': row.Suburb, 'Reserve': row.Reserve, 'Longitude': row.Longitude, 'Latitude': row.Latitude,
+       'Ward': row.Ward, 'Google': row.Google})
+    session.close()
+    return jsonify(data)
+
+@app.route('/api/geojson', methods = ['GET', 'POST'])
+def geojson():
+    data_path = "Data/picnic-settings.json"
+    data_geojson = json.load(open(data_path))
+    return jsonify(data_geojson)
 
 # You need this - this allows you to actually run the app
 if __name__ == "__main__":
